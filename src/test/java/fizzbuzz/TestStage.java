d55f454b84663da5377ab04dfe6be0a6ee658e1a
LuoHaoxian <d_anzel@163.com>
2019-04-04 16:48:55 +0800

    update test.

--- src/test/java/fizzbuzz/TestStage.java
+++ src/test/java/fizzbuzz/TestStage.java
@@ -53,16 +53,16 @@ public class TestStage {
     public void testBuzzRule() {
         BuzzRule buzzRule = new BuzzRule();
 
-        assertEquals(false,buzzRule.verify(1, false));
-        assertEquals(true,buzzRule.verify(5, false));
-        assertEquals(true,buzzRule.verify(10, false));
-        assertEquals(true,buzzRule.verify(15, false));
+        assertEquals(false,buzzRule.verify(1, false));//Not multiples of five return false
+        assertEquals(true,buzzRule.verify(5, false));//the multiples of five return true
+        assertEquals(true,buzzRule.verify(10, false));//the multiples of five  return true
+        assertEquals(true,buzzRule.verify(15, false));//the multiples of five  return true
 
-        assertEquals(false,buzzRule.verify(1, true));
-        assertEquals(true,buzzRule.verify(5, true));
-        assertEquals(true,buzzRule.verify(10, true));
-        assertEquals(true,buzzRule.verify(15, true));
-        assertEquals(true,buzzRule.verify(56, true));
+        assertEquals(false,buzzRule.verify(1, true));//Not multiples of five and without 5 in it
+        assertEquals(true,buzzRule.verify(5, true));//The multiples of five and has 5 in it
+        assertEquals(true,buzzRule.verify(10, true));//The multiples of five and without 5 in it
+        assertEquals(true,buzzRule.verify(15, true));//The multiples of five and has 5 in it
+        assertEquals(true,buzzRule.verify(56, true));//Not multiples of five and has 5 in it
 
     }
 
@@ -70,15 +70,15 @@ public class TestStage {
     public void testFizzRule() {
         FizzRule fizzRule = new FizzRule();
 
-        assertEquals(false,fizzRule.verify(1, false));
-        assertEquals(true,fizzRule.verify(3, false));
-        assertEquals(true,fizzRule.verify(6, false));
+        assertEquals(false,fizzRule.verify(1, false));//Not multiples of 3 return false
+        assertEquals(true,fizzRule.verify(3, false));//The multiples of 3 return true
+        assertEquals(true,fizzRule.verify(6, false));//The multiples of 3  rrturn true
 
-        assertEquals(false,fizzRule.verify(1, true));
-        assertEquals(true,fizzRule.verify(3, true));
-        assertEquals(true,fizzRule.verify(13, true));
-        assertEquals(true,fizzRule.verify(27, true));
-        assertEquals(true,fizzRule.verify(30, true));
+        assertEquals(false,fizzRule.verify(1, true));//Not multiples of 3 and without 3 in it
+        assertEquals(true,fizzRule.verify(3, true));//The multiples of 3 and has 3 in it
+        assertEquals(true,fizzRule.verify(13, true));//Not multiples of 3 and has 3 in it
+        assertEquals(true,fizzRule.verify(27, true));//The multiples of 3 and without 3 in it
+        assertEquals(true,fizzRule.verify(30, true));//The multiples of five and has 5 in it
 
     }
 
@@ -86,11 +86,11 @@ public class TestStage {
     public void testFizzBuzzRule() {
         FizzBuzzRule fizzBuzzRule = new FizzBuzzRule();
 
-        assertEquals(false,fizzBuzzRule.verify(1, false));
-        assertEquals(false,fizzBuzzRule.verify(20, false));
-        assertEquals(false,fizzBuzzRule.verify(27, false));
-        assertEquals(true,fizzBuzzRule.verify(15, false));
-        assertEquals(true,fizzBuzzRule.verify(30, false));
+        assertEquals(false,fizzBuzzRule.verify(1, false));//Not multiples of 3 and 5
+        assertEquals(false,fizzBuzzRule.verify(20, false));//multiples of 5 but 3
+        assertEquals(false,fizzBuzzRule.verify(27, false));//Not multiples of 3 but 5
+        assertEquals(true,fizzBuzzRule.verify(15, false));// multiples both of 3 and 5
+        assertEquals(true,fizzBuzzRule.verify(30, false));// multiples both of 3 and 5
 
         assertEquals(false,fizzBuzzRule.verify(1, true));
         assertEquals(true,fizzBuzzRule.verify(15, true));//divisible both 3 and 5 , And there are 5 in it
